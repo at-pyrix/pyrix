66c73d8d07e7d4d3917b655cded4c1aafb6cbff6
pyrix <90166733+at-pyrix@users.noreply.github.com>
2024-09-28 20:42:56 +0530

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,13 +8,13 @@
 	<p>'Sup?</p>
 	<p />
 	<p>
-		<strong>About me</strong><br> Hi! I'm @pyrix, a {new Date().getFullYear() - 2007} y/o guy from India. I'm interested in reading books about science, history and philosophy, graphic designing, drawing and tinkering with my computer. I want get into data visualisation, cybersecurity and networking, writing automation scripts, and scientific research. I want to learn everything about computers! <br><br> I started coding back in 2020, during the COVID-19 pandemic, when I taught myself python. This blog was supposed to be an online diary, where I would share my blog my development process and other stuff, but I never really got the time to write anything as I'm quite busy with school currently, and it's tough to manage both. Which is why, nearly all of the articles here are merely placeholders for actual content. Don't worry though, I'll get back here ASAP<s>
+		<strong>About me</strong><br><hr> Hi! I'm @pyrix, a {new Date().getFullYear() - 2007} y/o guy from India. I'm interested in reading books about science, history and philosophy, graphic designing, drawing and tinkering with my computer. I want get into data visualisation, cybersecurity and networking, writing automation scripts, and scientific research. I want to learn everything about computers! <br><br> I started coding back in 2020, during the COVID-19 pandemic, when I taught myself python. This blog was supposed to be an online diary, where I would share my blog my development process and other stuff, but I never really got the time to write anything as I'm quite busy with school currently, and it's tough to manage both. Which is why, nearly all of the articles here are merely placeholders for actual content. Don't worry though, I'll get back here ASAP<s>
 	</p>
         <p>
-                <strong>About this site</strong><br> I publish the articles from a CMS <a href="sanity.io">Sanity</a>. Honestly there's a lot of work left to do on this site, so it's quite janky at times. And now that I look back, some of the elements here really cringe me out lmao
+                <strong>About this site</strong><br><hr> I publish the articles from a CMS <a href="sanity.io">Sanity</a>. Honestly there's a lot of work left to do on this site, so it's quite janky at times. And now that I look back, some of the elements here really cringe me out lmao
         </p>
 	<p>
-<strong>future plans</strong><br>
+<strong>future plans</strong><br><hr>
 		With all the AI stuff going on currently, I'm quite pessimistic about the future of devs lol. However, I still love coding and when I'm back, I'm planning to get into Android app development with the MDY guidelines and want to do more privacy-friendly FOSS projects in the future.
 		<br />
 		<br />
