548c633b6b436504a2128ae6860e2301a9663052
pyrix <90166733+at-pyrix@users.noreply.github.com>
2024-09-28 21:25:48 +0530

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -63,9 +63,10 @@
         h3 {
 font-family: 'Monument Extended', sans-serif !important;
 text-transform: uppercase;
-font-size: 1.3rem;
+font-size: 1.2rem;
 color: hsl(170, 36%, 55%) !important;
 margin-top: 2rem;
+margin-top: 1.5rem;
         }
 
 	.title-line {
